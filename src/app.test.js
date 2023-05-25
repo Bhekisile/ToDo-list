7676e2be488c0eafdd05035da0a2c62c030a123d
Miki <ghermaico135@gmail.com>
2023-05-25 15:48:55 +0300

    Add addTask test

--- src/app.test.js
+++ src/app.test.js
@@ -1,29 +1,39 @@
-const {
-  tasks,
-  addNewTask,
-  deleteTask,
-} = require('./app.js');
+/** @format */
+
+const { tasks, addNewTask, deleteTask } = require("./app.js");
 
 const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn(),
+	getItem: jest.fn(),
+	setItem: jest.fn(),
+	clear: jest.fn(),
 };
 
 beforeEach(() => {
-  jest.clearAllMocks();
-  Object.defineProperty(window, 'localStorage', {
-    value: localStorageMock,
-    configurable: true,
-  });
+	jest.clearAllMocks();
+	Object.defineProperty(window, "localStorage", {
+		value: localStorageMock,
+		configurable: true,
+	});
 });
 
-describe('deleteTask', () => {
-  it('deletes a task from the list', () => {
-    const initialTasksLength = tasks.length;
-    const taskIndex = 0;
-    deleteTask(taskIndex);
-    expect(tasks.length).toBe(initialTasksLength - 1);
-    expect(tasks.every((task, index) => task.index === index)).toBe(true);
-  });
+describe("Task list app", () => {
+	describe("addNewTask", () => {
+		it("adds a new task to the list", () => {
+			const initialTasksLength = tasks.length;
+			const taskDescription = "Test tasks description";
+			addNewTask(taskDescription);
+			expect(tasks.length).toBe(initialTasksLength + 1);
+			expect(tasks[initialTasksLength].description).toBe(taskDescription);
+		});
+	});
+});
+
+describe("deleteTask", () => {
+	it("deletes a task from the list", () => {
+		const initialTasksLength = tasks.length;
+		const taskIndex = 0;
+		deleteTask(taskIndex);
+		expect(tasks.length).toBe(initialTasksLength - 1);
+		expect(tasks.every((task, index) => task.index === index)).toBe(true);
+	});
 });
